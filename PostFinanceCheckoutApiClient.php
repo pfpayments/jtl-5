0f0259aafbd40fdd704c7988ab46e7db5ba8ec33
Drew Rowan <andrew.rowan@wallee.com>
2023-10-02 10:42:30 +0200

    Release 1.0.2

--- PostFinanceCheckoutApiClient.php
+++ PostFinanceCheckoutApiClient.php
@@ -43,7 +43,7 @@ class PostFinanceCheckoutApiClient
 			
 			try {
 				$apiClient = new ApiClient($userId, $applicationKey);
-				$apiClientBasePath = getenv('POSTFINANCECHECKOUT_API_BASE_PATH') ? getenv('POSTFINANCECHECKOUT_API_BASE_PATH') : $this->apiClient->getBasePath();
+				$apiClientBasePath = getenv('POSTFINANCECHECKOUT_API_BASE_PATH') ? getenv('POSTFINANCECHECKOUT_API_BASE_PATH') : $apiClient->getBasePath();
 				$apiClient->setBasePath($apiClientBasePath);
 				foreach (self::getDefaultHeaderData() as $key => $value) {
 					$apiClient->addDefaultHeader($key, $value);
