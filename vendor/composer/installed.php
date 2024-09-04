ee134a98350f7bfc316bcb2e8f74ed27329e0d9a
Edgaras <edgaras.tkacenka@wallee.com>
2024-09-04 10:23:11 +0300

    Release 1.0.29

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '23881441945da62c99ed76e8ccae1abaa8c573cc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '23881441945da62c99ed76e8ccae1abaa8c573cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'postfinancecheckout/sdk' => array(
+            'pretty_version' => '3.2.0',
+            'version' => '3.2.0.0',
+            'reference' => '278ee4dbb9765a7946e58bf89dc8125b8dd8113b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../postfinancecheckout/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
